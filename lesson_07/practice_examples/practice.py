1b960f75fda297d6304d5067dff293232d0088b3
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-22 16:18:23 -0500

    Added practice examples

---
+++ lesson_07/practice_examples/practice.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Input
+
+# favorite_food = input("What is your favorite food? ")
+# print("Your favorite food is " + favorite_food + ".")
+
+# Int and Float
+
+# decimal = float(5)
+# not_decimal = int(5)
+# print(decimal)
+# print(not_decimal)
+
+# String interpolation
+
+# normal = "This string is"
+# fancy = "fancy"
+# print(normal + " " + fancy + ".")
+# print(f"{normal} {fancy}.")
+
+# Typecasting
+
+# num_to_str = str(45)
+# str_to_num = int("45")
+# print(num_to_str)
+# print(str_to_num)
+# print(type(num_to_str))
+# print(type(str_to_num))
+
+# Boolean
+
+# this_is_true = True
+# this_is_false = False
+# print(f"{this_is_true} {this_is_false}")
+# print(f"{type(this_is_true)} {type(this_is_false)}")
+
+# Math and ABS
+
+my_sum = 3 + 4
+difference = 3-4
+product = 3 * 4
+quotient = 3 / 4
+quotient_two = 3 // 4
+abs_value = abs(-5)
+print(my_sum)
+print(difference)
+print(product)
+print(quotient)
+print(quotient_two)
+print(abs_value)
